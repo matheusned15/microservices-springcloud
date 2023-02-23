e2d2e8a09f9eb31167138e4b291d858c84de458e
Matheus Ned <matheusned15@gmail.com>
2023-02-23 14:59:04 -0300

    Concluindo e testando o microservice de cliente

--- msclientes/src/main/java/io/github/matheusned/msclientes/application/ClientesResource.java
+++ msclientes/src/main/java/io/github/matheusned/msclientes/application/ClientesResource.java
@@ -1,15 +1,45 @@
 package io.github.matheusned.msclientes.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.matheusned.msclientes.application.representation.ClienteSaveRequest;
+import io.github.matheusned.msclientes.domain.Cliente;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("clientes")
+@RequiredArgsConstructor
 public class ClientesResource {
+
+    private final ClienteService service;
 
     @GetMapping
     public String status() {
         return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody ClienteSaveRequest request) {
+
+        var cliente = request.toModel();
+        service.save(cliente);
+        URI headerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(cliente.getCpf())
+                .toUri();
+        return ResponseEntity.created(headerLocation).build();
+    }
+
+    @GetMapping
+    public ResponseEntity dadosCliente(String cpf) {
+        var cliente = service.getByCPF(cpf);
+        if (cliente.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cliente);
     }
 }
---
+++ msclientes/src/main/java/io/github/matheusned/msclientes/application/representation/ClienteSaveRequest.java
@@ -0,0 +1,15 @@
+package io.github.matheusned.msclientes.application.representation;
+
+import io.github.matheusned.msclientes.domain.Cliente;
+import lombok.Data;
+
+@Data
+public class ClienteSaveRequest {
+    private String cpf;
+    private String nome;
+    private Integer idade;
+
+    public Cliente toModel() {
+        return new Cliente(cpf, nome, idade);
+    }
+}
