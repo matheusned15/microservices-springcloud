37a8f67d58c5f13782612b5d3b0b40154b8fcd9d
Matheus Ned <matheusned15@gmail.com>
2023-02-22 23:21:43 -0300

    Implementando o microservice de cliente

---
+++ msclientes/src/main/java/io/github/matheusned/msclientes/application/ClienteService.java
@@ -0,0 +1,26 @@
+package io.github.matheusned.msclientes.application;
+
+import io.github.matheusned.msclientes.domain.Cliente;
+import io.github.matheusned.msclientes.infra.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteService {
+
+    private final ClienteRepository repository;
+
+    @Transactional
+    public Cliente save(Cliente cliente){
+        return repository.save(cliente);
+    }
+
+    public Optional<Cliente> getByCPF(String cpf){
+        return repository.findByCpf(cpf);
+    }
+}
